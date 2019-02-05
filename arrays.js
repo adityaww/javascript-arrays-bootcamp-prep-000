57ac773055aa5b3e5a0db63bc6a1f6dad5cad441
Aditya Waruwu <aditya.william96@gmail.com>
2019-02-05 03:56:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,5 +3,6 @@ function addElementToBeginningOfArray(array, element) {
   return [element,...array];
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift
+  return array.unshift(element);
+  return array
 }

36c605adca6ec1ea2f2955470561cd3cf2043fc0
Aditya Waruwu <aditya.william96@gmail.com>
2019-02-05 03:55:08 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,4 +2,6 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(array, element) {
   return [element,...array];
 }
-destructivelyAddElementToBeginningOfArray(array, element)
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return []
+}
